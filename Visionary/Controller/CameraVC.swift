77b20a68ba8c1ca922a9e88b3b6f8249ccb1521b
Sebastian C <crossasebastian@gmail.com>
2018-10-07 20:31:27 -0500

    Added camera support

--- Visionary/Controller/CameraVC.swift
+++ Visionary/Controller/CameraVC.swift
@@ -7,9 +7,16 @@
 //
 
 import UIKit
+import AVFoundation
 
 class CameraVC: UIViewController {
 
+    // MARK : Variable declarations
+    var captureSession: AVCaptureSession!
+    var output:AVCapturePhotoOutput!
+    var previewLayer: AVCaptureVideoPreviewLayer!
+    
+    // MARK : Outlet connections
     @IBOutlet weak var capturedImageView: RoundedShadowImageView!
     @IBOutlet weak var flashButton: RoundedShadowButton!
     @IBOutlet weak var itemIdentification: UILabel!
@@ -20,6 +27,46 @@ class CameraVC: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        previewLayer.frame = cameraView.bounds
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        let backCamera = AVCaptureDevice.default(for: AVMediaType.video)
+        
+        captureSession = AVCaptureSession()
+        captureSession.sessionPreset = AVCaptureSession.Preset.hd1920x1080
+        
+        // When using the camera you always have to try to catch errors, since they are prominent here
+        do {
+            let input = try AVCaptureDeviceInput(device: backCamera!)
+            
+            if captureSession.canAddInput(input) == true {
+                captureSession.addInput(input)
+            }
+            
+            output = AVCapturePhotoOutput()
+            
+            if captureSession.canAddOutput(output) == true {
+                captureSession.addOutput(output!)
+                
+                previewLayer = AVCaptureVideoPreviewLayer(session: captureSession)
+                previewLayer.videoGravity = AVLayerVideoGravity.resizeAspect // Similar to the content mode in an imageView
+                previewLayer.connection?.videoOrientation = .portrait
+                
+                cameraView.layer.addSublayer(previewLayer!)
+                captureSession.startRunning()
+            }
+        } catch {
+            debugPrint(error)
+        }
         
     }
     
