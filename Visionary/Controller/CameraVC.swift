c8e42930120f470d97f1f1e4b2bfc8d4907ceb56
Sebastian C <crossasebastian@gmail.com>
2018-10-07 19:01:26 -0500

    Project setup

---
+++ Visionary/Controller/CameraVC.swift
@@ -0,0 +1,20 @@
+//
+//  CameraVC.swift
+//  Visionary
+//
+//  Created by Sebastian Crossa on 10/7/18.
+//  Copyright © 2018 Sebastian Crossa. All rights reserved.
+//
+
+import UIKit
+
+class CameraVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+    }
+
+}
+
